6ac5023993ab310bf8e371ab52ee5c35a8a9704a
Ed Hawkins <30795989+ed-hawkins@users.noreply.github.com>
2022-01-28 16:22:09 +0000

    Tweaks to plotting script and added one replacement data file

--- plotting_script.m
+++ plotting_script.m
@@ -1,17 +1,18 @@
 clear all
 close all
 dfiles={'AMOC','NAO','JET-JJA','JET-DJF','SPG-OHC','ARCTIC-SEA-ICE'};
-vname={'AMOC [Sv]','NAO (DJFM)','Jet latitude (JJA) [°N]','Jet speed (DJF) [m/s]','Sub-polar gyre OHC [ZJ]','Arctic autumn sea ice volume [km^3]'};
+vname={'AMOC [Sv]','NAO (DJFM)','Atlantic jet latitude (JJA) [°N]','Atlantic jet speed (DJF) [m/s]','Atlantic sub-polar gyre OHC [ZJ]','Arctic autumn sea ice volume [km^3]'};
 figure
 for dd=1:length(dfiles)
     D=load(sprintf('DATA/%s.txt',dfiles{dd}));
     subaxis(length(dfiles),1,dd,'SV',0.05);
     plot(D(:,1),D(:,2),'LineWidth',2);
     xlim([1949 2021]);
-    ylabel(vname{dd},'FontSize',11);
-    set(gca,'FontSize',13);
+    TT=title(vname{dd},'FontWeight','normal');
+    set(gca,'FontSize',14);
+    set(TT,'FontSize',20);
 end
 set(gcf,'PaperPosition',[0 0 25 8*length(dfiles)]);
-print('-dpng','ACSIS-indicators.png');
+print('-dpng','-r600','ACSIS-indicators.png');
 
 
